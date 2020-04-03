dc6b0de5601af9ebe070fe4846df973d7fbf758a
Leo Eiji <leoeiji1234@gmail.com>
2020-04-03 18:13:22 -0300

    Teste

--- Example.cc
+++ Example.cc
@@ -48,11 +48,11 @@ int main(int argc,char** argv)
   runManager->SetUserInitialization(detector);
 
   // Local user Physics List
-  // G4VUserPhysicsList* physics = new PhysicsList();
+  G4VUserPhysicsList* physics = new PhysicsList();
   // G4VModularPhysicsList* physics1 = new QGSP_BERT();
   // Reference Physics List from Geant4 kernel
   // G4VUserPhysicsList* physics )= new QGSP_BERT();
-  G4VUserPhysicsList* physics = new FTFP_BERT();
+  // G4VUserPhysicsList* physics = new FTFP_BERT();
 
 
   // mandatory User class
